75891b992416eaf86bbc8c7719ca30e5418142ed
Liam Hess <liamskydamien@gmail.com>
2023-01-02 18:55:52 +0100

    Add useEffect

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,21 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+      const [isLoggedIn, setIsLoggedIn] = useState(false);
+      const loginHandler = (email, password) => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo anyways
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+      };
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
+      const logoutHandler = () => {
+        setIsLoggedIn(false);
+      };
 
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
+      useEffect(() => {
+          localStorage.getItem('isLoggedIn') === '1' ? setIsLoggedIn(true) : setIsLoggedIn(false);
+      }, []);
 
   return (
     <React.Fragment>
